ead9ed222a21b0158ac422d54e2f6aa430157416
Oliver Findl <github@findl.sk>
2018-05-16 18:46:01 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # node-spotilocal
-Simple wrapper for Spotify app local webserver.
+
+![license](https://img.shields.io/github/license/oliverfindl/node-spotilocal.svg?style=flat)
+[![paypal](https://img.shields.io/badge/donate-paypal-blue.svg?colorB=0070ba&style=flat)](https://paypal.me/oliverfindl)
+
+Simple wrapper for [Spotify](https://www.spotify.com/) app local webserver.
+
+---
+
+## Install
+
+Via [npm](https://npmjs.com/) [[package](https://www.npmjs.com/package/node-spotilocal)]:
+> `npm install node-spotilocal`
+
+Via [yarn](https://yarnpkg.com/en/) [[package](https://yarnpkg.com/en/package/node-spotilocal)]:
+> `yarn add node-spotilocal`
+
+## Usage
+
+``` javascript
+// import lib
+import * from "node-spotilocal";
+
+// init lib
+const spotify = new Spotilocal();
+
+// play track
+spotify.play("spotify:track:1qCQTy0fTXerET4x8VHyr9").then(console.log).catch(console.error);
+
+// pause track
+spotify.pause().then(console.log).catch(console.error);
+
+// unpause track
+spotify.unpause().then(console.log).catch(console.error);
+
+// get current status
+spotify.status().then(console.log).catch(console.error);
+```
+
+---
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ src/index.js
@@ -0,0 +1,181 @@
+/**
+ * node-spotilocal v1.0.0 (2018-05-16)
+ * Copyright 2018 Oliver Findl
+ * @license MIT
+ */
+
+"use strict";
+
+const { sampleSize } = require("lodash");
+const { get } = require("axios");
+
+export default class Spotilocal {
+
+	constructor() {
+		this.ssl = false;
+		this.protocol = "";
+		this.hostname = "";
+		this.port = 0;
+		this.csrf = "";
+		this.oauth = "";
+	}
+
+	_ssl() {
+		// 4370 - 4379 = ssl
+		// 4380 - 4389 = no ssl
+		return this.ssl = parseInt(this.port) < 4380;
+	}
+
+	_protocol() {
+		return this.protocol = "http" + (this._ssl() ? "s" : "");
+	}
+
+	_hostname() {
+		if(this._ssl()) {
+			let str = "";
+			for(let i = 97; i <= 122; i++) str += String.fromCharCode(i);
+			this.hostname = sampleSize(str, 10).join("") + ".spotilocal.com";
+		} else this.hostname = "localhost";
+		return this.hostname;
+	}
+
+	async _port(port = 0) {
+		const limit = Object.freeze({
+			min: 4370,
+			max: 4389
+		});
+
+		port = parseInt(port);
+		if(!port || port < limit.min) port = limit.min;
+		else if(port > limit.max) port = limit.max;
+
+		for(this.port = port; this.port < limit.max; this.port++) {
+			try {
+				console.log("Testing port: %d", this.port);
+				await this._version();
+				console.log("Port found: %d", this.port);
+				return this.port;
+			} catch(err) {
+				// throw err;
+				if(this.port >= limit.max) throw new Error("Port not found!");
+			}
+		}	
+	}
+
+	_url(path = "") {
+		return `${this._protocol()}://${this._hostname()}:${this.port}${path}`;
+	}
+
+	async _json(url = "", params = {}) {
+		try {
+			const res = await get(url, {
+				params,
+				headers: {
+					origin: "https://open.spotify.com"
+				}
+			});
+			if(res.hasOwnProperty("status") && [200, 304].indexOf(res.status) > -1 && res.hasOwnProperty("data")) return res.data;
+			else throw new Error("Bad response!");
+		} catch(err) {
+			throw err;
+		}
+	}
+
+	async _version() {
+		try {
+			return await this._json(this._url("/service/version.json"), {
+				service: "remote"
+			});
+		} catch(err) {
+			throw err;
+		}
+	}
+
+	async _open() {
+		try {
+			return await this._json(this._url("/remote/open.json"));
+		} catch(err) {
+			throw err;
+		}
+	}
+
+	async _csrf() {
+		try {
+			const res = await this._json(this._url("/simplecsrf/token.json"));
+			if(res.hasOwnProperty("token")) return this.csrf = res.token;
+			else throw new Error("Missing CSRF token!");
+		} catch(err) {
+			throw err;
+		}
+	}
+
+	async _oauth() {
+		try {
+			const res = await this._json("https://open.spotify.com/token");
+			if(res.hasOwnProperty("t")) return this.oauth = res.t;
+			else throw new Error("Missing OAuth token!");
+		} catch(err) {
+			throw err;
+		}
+	}
+
+	async _auth() {
+		if(this.csrf && this.oauth) return {
+			csrf: this.csrf,
+			oauth: this.oauth
+		};
+
+		try {
+			return await this._port() && await this._open() && await this._csrf() && await this._oauth() && {
+				csrf: this.csrf,
+				oauth: this.oauth
+			};
+		} catch(err) {
+			throw err;
+		}
+	}
+
+	async status() {
+		try {
+			return await this._auth() && await this._json(this._url("/remote/status.json"), {
+				csrf: this.csrf,
+				oauth: this.oauth,
+				returnafter: 0,
+				returnon: ["login", "logout", "play", "pause", "error", "ap"].join(",")
+			});
+		} catch(err) {
+			throw err;
+		}
+	}
+
+	async play(spotifyUri = "") {
+		if(!/^spotify:track:[\w\d]+$/.test(spotifyUri)) throw new Error("Bad Spotify track URI!");
+		try {
+			return await this._auth() && await this._json(this._url("/remote/play.json?action=queue"), {
+				csrf: this.csrf,
+				oauth: this.oauth,
+				uri: spotifyUri,
+				context: spotifyUri
+			});
+		} catch(err) {
+			throw err;
+		}
+	}
+
+	async pause(pause = true) {
+		try {
+			return await this._auth() && await this._json(this._url("/remote/pause.json"), {
+				csrf: this.csrf,
+				oauth: this.oauth,
+				pause: pause ? true : false
+			});
+		} catch(err) {
+			throw err;
+		}
+	}
+
+	async unpause() {
+		return this.pause(false);
+	}
+
+}
